804268e1916b7e47568905caa8a12ae6ee9ff2ed
🅁Øtxz7 <rodrigoporto.c2010@gmail.com>
2025-09-04 20:00:28 -0300

    Field Centric /Rotxz

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain/FieldCentric.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode.DriveTrain;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class FieldCentric extends LinearOpMode {
 
     private DcMotor RMF, RMB, LMF, LMB;
+    private IMU imu;
     private double speed = 0.75;
 
     @Override
@@ -20,6 +25,15 @@ public class FieldCentric extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
+            imu();
+            drive();
+
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            telemetry.addData("IMU", imu.getRobotYawPitchRollAngles());
+            telemetry.update();
         }
 
     }
@@ -30,6 +44,54 @@ public class FieldCentric extends LinearOpMode {
         LMF = hardwareMap.get(DcMotor.class, "LMF");
         LMB = hardwareMap.get(DcMotor.class, "LMB");
 
+        imu = hardwareMap.get(IMU.class, "imu");
+
+    }
+
+    public void imu() {
+
+        RevHubOrientationOnRobot RobotOrientaion = new RevHubOrientationOnRobot(
+          RevHubOrientationOnRobot.LogoFacingDirection.DOWN,
+          RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD
+        );
+
+        imu.initialize(new IMU.Parameters(RobotOrientaion));
+
+    }
+
+    public void drive() {
+        double drive = - gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x;
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+
+        double driveY = strafe * Math.sin(-botHeading) + drive * Math.cos(-botHeading);
+        double strafeX = strafe * Math.cos(-botHeading) - drive * Math.sin(-botHeading);
+
+        strafeX = strafeX * 1.1;
+
+        driveY = apllyDeadzone(driveY);
+        strafeX = apllyDeadzone(strafeX);
+        turn = apllyDeadzone(turn);
+
+        double denominator = Math.max(Math.abs(driveY) + Math.abs(strafeX) + Math.abs(turn), 1);
+        double powerRMF = (driveY - strafeX - turn) / denominator;
+        double powerRMB = (driveY + strafeX - turn) / denominator;
+        double powerLMF = (driveY + strafeX + turn) / denominator;
+        double powerLMB = (driveY - strafeX + turn) / denominator;
+
+        RMF.setPower(powerRMF * speed);
+        RMB.setPower(powerRMB * speed);
+        LMF.setPower(powerLMF * speed);
+        LMB.setPower(powerLMB * speed);
+
+    }
+
+    private double apllyDeadzone(double value) {
+        return Math.abs(value) > 0.05 ? 0 : value;
     }
 
 }
+
+//Rotxz

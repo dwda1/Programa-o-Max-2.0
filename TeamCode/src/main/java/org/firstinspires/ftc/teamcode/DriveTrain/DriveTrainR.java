e2aebc2ad510a875beb323beb44b3069018ebe1b
🅁Øtxz7 <rodrigoporto.c2010@gmail.com>
2025-09-04 19:09:27 -0300

    DriveTrain testeR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain/DriveTrainR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain/DriveTrainR.java
@@ -2,15 +2,27 @@ package org.firstinspires.ftc.teamcode.DriveTrain;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class DriveTrainR extends LinearOpMode {
 
     private DcMotor RMF, RMB, LMF, LMB;
+    private double speed = 0.75;
 
     @Override
     public void runOpMode() {
         initHardware();
+
+        RMF.setDirection(DcMotorSimple.Direction.REVERSE);
+        RMB.setDirection(DcMotorSimple.Direction.REVERSE);
+        LMF.setDirection(DcMotorSimple.Direction.FORWARD);
+        LMB.setDirection(DcMotorSimple.Direction.FORWARD);
+
         waitForStart();
+        while (opModeIsActive()) {
+            drive();
+
+        }
 
     }
 
@@ -27,6 +39,26 @@ public class DriveTrainR extends LinearOpMode {
         double strafe = gamepad1.left_stick_x * 1.1;
         double turn = gamepad1.right_stick_x;
 
+        drive = apllyDeadzone(drive);
+        strafe = apllyDeadzone(strafe);
+        turn = apllyDeadzone(turn);
+
+        double denominator = Math.max(Math.abs(drive) + Math.abs(strafe) + Math.abs(turn), 1);
+
+        double powerRMF = (drive - strafe - turn) / denominator;
+        double powerRMB = (drive + strafe - turn) / denominator;
+        double powerLMF = (drive + strafe + turn) / denominator;
+        double powerLMB = (drive - strafe + turn) / denominator;
+
+        RMF.setPower(powerRMF * speed);
+        RMB.setPower(powerRMB * speed);
+        LMF.setPower(powerLMF * speed);
+        LMB.setPower(powerLMB * speed);
+
+    }
+
+    private double apllyDeadzone(double value) {
+       return Math.abs(value) > 0.05 ? 0 : value;
     }
 
 }

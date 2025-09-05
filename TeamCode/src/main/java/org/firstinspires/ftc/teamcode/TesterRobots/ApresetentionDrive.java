d94a0c09f1f68d3893b15a4861ae39ad39756949
🅁Øtxz7 <rodrigoporto.c2010@gmail.com>
2025-09-05 17:11:33 -0300

    ApresetentionDrive /Rotxz

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesterRobots/ApresetentionDrive.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.TesterRobots;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class ApresetentionDrive extends LinearOpMode {
+
+    private DcMotor RMF, RMB, LMF, LMB;
+    private DcMotor Arm;
+    private double speed = 0.75;
+
+    @Override
+    public void runOpMode() {
+        initHardware();
+
+        RMF.setDirection(DcMotorSimple.Direction.FORWARD);
+        RMB.setDirection(DcMotorSimple.Direction.FORWARD);
+        LMF.setDirection(DcMotorSimple.Direction.REVERSE);
+        LMB.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            drive();
+            Arm1();
+
+//            telemetry.addData("power RMF", RMF.getPower());
+//            telemetry.addData("power RMB", RMB.getPower());
+//            telemetry.addData("power LMF", LMF.getPower());
+//            telemetry.addData("power LMB", LMB.getPower());
+            telemetry.update();
+        }
+
+    }
+
+    private void initHardware() {
+        RMF = hardwareMap.get(DcMotor.class, "RMF");
+        RMB = hardwareMap.get(DcMotor.class, "RMB");
+        LMF = hardwareMap.get(DcMotor.class, "LMF");
+        LMB = hardwareMap.get(DcMotor.class, "LMB");
+        Arm = hardwareMap.get(DcMotor.class, "Arm");
+
+    }
+
+    private void drive() {
+        double drive = -gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x * 1.1;
+        double turn = gamepad1.right_stick_x;
+
+        drive = apllyDeadzone(drive);
+        strafe = apllyDeadzone(strafe);
+        turn = apllyDeadzone(turn);
+
+        double denominator = Math.max(Math.abs(drive) + Math.abs(strafe) + Math.abs(turn), 1);
+
+        double powerRMF = (drive - strafe - turn) / denominator;
+        double powerRMB = (drive + strafe - turn) / denominator;
+        double powerLMF = (drive + strafe + turn) / denominator;
+        double powerLMB = (drive - strafe + turn) / denominator;
+
+        RMF.setPower(powerRMF * speed);
+        RMB.setPower(powerRMB * speed);
+        LMF.setPower(powerLMF * speed);
+        LMB.setPower(powerLMB * speed);
+
+    }
+
+    private void Arm1() {
+
+        if (gamepad1.right_bumper) {
+            Arm.setPower(1);
+        } else {
+            Arm.setPower(0);
+        }
+
+    }
+
+    private double apllyDeadzone(double value) {
+        return Math.abs(value) > 0.05 ? value : 0;
+    }
+
+}
+
+//Rotxz
